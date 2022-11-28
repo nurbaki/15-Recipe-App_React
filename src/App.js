c7ffa0951e3a3b630f9688b9af88cb77cd3770d7
nurbaki <nurbakibayansal11@gmail.com>
2022-11-28 14:15:59 +0100

    app

--- src/App.js
+++ src/App.js
@@ -21,15 +21,3 @@ const App = () => {
 };
 
 export default App;
-
-// import AppRouter from "./router/AppRouter";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <AppRouter />
-//     </div>
-//   );
-// }
-
-// export default App;
